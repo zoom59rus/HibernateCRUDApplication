c086fa9ca554001c6d569d02657b46957695335d
zoom59rus <anton_1@bk.ru>
2021-02-26 12:18:16 +0500

    Project complete.

--- src/main/java/com/nazarov/javadeveloper/chapter23/ApplicationRunner.java
+++ src/main/java/com/nazarov/javadeveloper/chapter23/ApplicationRunner.java
@@ -1,16 +1,6 @@
 package com.nazarov.javadeveloper.chapter23;
 
-import com.nazarov.javadeveloper.chapter23.entity.Post;
-import com.nazarov.javadeveloper.chapter23.entity.Region;
-import com.nazarov.javadeveloper.chapter23.entity.Writer;
-import com.nazarov.javadeveloper.chapter23.repository.WriterRepository;
-import com.nazarov.javadeveloper.chapter23.repository.impl.WriterRepositoryImpl;
-import com.nazarov.javadeveloper.chapter23.service.UserService;
-import com.nazarov.javadeveloper.chapter23.service.impl.UserServiceImpl;
 import com.nazarov.javadeveloper.chapter23.view.UserMenu;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ApplicationRunner {
 
@@ -18,23 +8,5 @@ public class ApplicationRunner {
         Context context = ApplicationContext.init();
         UserMenu userMenu = UserMenu.getInstance();
         userMenu.showMainMenu();
-
-//        UserService userService = new UserServiceImpl();
-//
-//        Writer result = userService.getByLastName("Nazarov");
-//        System.out.println(result);
-
-
-//        Post post = new Post("Simple post");
-//
-//        Region region = new Region("Perm");
-//        Writer writer = new Writer("Anton", "Nazarov", region);
-//        List<Post> posts = new ArrayList<>();
-//        posts.add(new Post("Some content #1"));
-//        posts.add(new Post("Some content #2"));
-//        writer.setPosts(posts);
-//
-//        Writer result = writerRepository.save(writer);
-//        System.out.println(result);
     }
 }
